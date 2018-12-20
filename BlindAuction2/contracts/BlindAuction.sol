9c2ecf4ace1613030b846f5ae893745df9b81ae7
SimYi <simyixiao@gmail.com>
2018-12-20 20:47:00 +0800

    DEC 20 2018 20:50

--- BlindAuction2/contracts/BlindAuction.sol
+++ BlindAuction2/contracts/BlindAuction.sol
@@ -3,12 +3,16 @@ contract BlindAuction{
 
 	mapping(address => bool) public isBidded;
 	mapping(uint => Bidder) public bidders;
+	mapping(address => bool) public isRefunded;
 
 	address public beneficiary;
 	uint public duration;
 	uint public reserve;
 	uint public endAuction;
 	uint public bidderNum;
+	uint public hightestBid;
+	uint public winner;
+	uint bool isEnd;
 
 	struct Bidder{
 		address bidderAddress;
@@ -26,12 +30,28 @@ contract BlindAuction{
 
 	//bid
 	function bid(uint price) public payable{
+		require(!isEnd);
 		require(!isBidded[msg.sender]);
 		require(reservePrice >= reserve);
 		require(now <= endAuction);
 		isBidded[msg.sender] = true;
-		bidders[bidderNum ++] = Bidder(msg.sender, price, msg.value);
+		bidders[bidderNum] = Bidder(msg.sender, price, msg.value);
+		if(price > hightestBid){
+			hightestBid = price;
+			winner = bidderNum;
+		}
+		bidderNum ++;
 	}
 
 	//AuctionEnd
+	function auctionEnd() public{
+		require(!isEnd);
+		require(now > endAuction);
+		Bidder tmpBidder;
+		isRefunded(bidders(winner).bidderAddress) = true;
+		for(uint i = 0;(i <= bidderNum) && (!isRefunded(bidders(i).bidderAddress));i ++){
+			tmpBidder = bidders(i);
+			tmpBidder.bidderAddress.transfer(tmpBidder.reservePrice);
+		}
+	}
 }
